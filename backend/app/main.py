66dd72e98a504d46802793b9b314f7c31b6c87e9
kazu744 <oyamakazuma1@gmail.com>
2025-07-05 18:24:42 +0900

    add template

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,21 @@
-from fastapi import FastAPI, File, UploadFile
-import shutil
-from pathlib import Path
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-UPLOAD_DIR = Path(r"C:\OCR\uploads")
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
 
+@app.get('/upload', response_class=HTMLResponse)
+def get_upload_page(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
+
 @app.post('/upload')
 async def upload(file: UploadFile = File(...)):
     contents = await file.read()
-
     return {"filename": file.filename, "size": len(contents)}
 
 if __name__ == "__main__":

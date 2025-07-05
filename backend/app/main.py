6c1c423b1f20fedc62d983a159aefaf89c18a28f
kazu744 <oyamakazuma1@gmail.com>
2025-07-05 16:33:03 +0900

    add upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+from pathlib import Path
 
 app = FastAPI()
+
+UPLOAD_DIR = Path(r"C:\OCR\uploads")
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+@app.post('/upload')
+async def upload(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    return {"filename": file.filename, "size": len(contents)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
